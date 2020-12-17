332604cfbdd006a0ecb0ef480835c8ef81a80c07
isgleb <71262423+isgleb@users.noreply.github.com>
2020-12-17 22:56:55 +0300

    services added

--- src/main/resources/static/js/new-order.js
+++ src/main/resources/static/js/new-order.js
@@ -71,6 +71,24 @@ $(document).ready(function() {
 //                    error: error
             });
 
+
+
+
+//            $.ajax({
+//                url: '/positions/request?' + $.param({orderId: ordersId}),
+//                type: 'POST',
+//                contentType: "application/json",
+//                data: JSON.stringify(position),
+//                success: success,
+//                error: error
+//            });
+
+
+
+
+
+
+
             window.location.replace(orderPage);
 
         });
@@ -235,21 +253,21 @@ $(document).ready(function() {
         responsive: true,
         altEditor: true,     // Enable altEditor
         buttons: [
-                {
-                text: 'Add',
-                name: 'add',        // do not change name
-                },
-                {
+            {
+                text: 'Новый',
+                name: 'add'        // do not change name
+            },
+            {
                 extend: 'selected', // Bind to Selected row
-                text: 'Edit',
+                text: 'Изменить данные',
                 name: 'edit'        // do not change name
-                },
-
-                {
+            },
+            {
                 extend: 'selected', // Bind to Selected row
-                text: 'Delete',
+                text: 'Удалить',
                 name: 'delete'      // do not change name
-                }],
+            }
+        ],
         onAddRow: function(datatable, rowdata, success, error) {
 
                 newId = "new" + invoiceFrontId.toString();
@@ -292,147 +310,6 @@ $(document).ready(function() {
       });
 
 
-//
-//
-//
-//    var services;
-//    var serviceMap = new Map();
-//
-//    $.ajax({
-//            type: 'GET',
-//            url: "/services/request",
-//            async: false,
-//            success: function (response) {services = response;}
-//            });
-//
-//    services.forEach(function(service){
-//        serviceMap.set(service.id, service.serviceType);
-//        })
-//
-//    var servicesOptions = Object.fromEntries(serviceMap.entries());
-//
-//
-////    $.ajax({
-////          type: 'GET',
-////          url: '/order/customer?' + $.param({orderId: ordersId}),
-////          async: false,
-////          success: function (response) {customer = response;}
-////          });
-//
-//    var columnDefs = [
-//
-//        {
-//        data: "id",
-//        title: "id",
-//        type: "readonly"
-//        },
-//        {
-//        data: "serviceId",
-//        title: "Услуга",
-//        type : "select",
-//        options : servicesOptions,
-//        select1 : { width: "100%"},
-//        render: function (data, type, row, meta) {
-//
-//            if (data == null || !(data in servicesOptions)) {
-//            return null;
-//            }
-//
-//            return servicesOptions[data];
-//            }
-//        },
-//        {
-//        data: "quantity",
-//        title: "Количество",
-//        type: "number",
-//        required: true,
-//        },
-//        {
-//        data: "totalPrice",
-//        title: "сумма",
-//        },
-//    ];
-//
-//    var serviceTable;
-//
-//    serviceTable = $('#positions').DataTable({
-//        "sPaginationType": "full_numbers",
-//        ajax: {
-//            url : '/positions/request?' + $.param({orderId: ordersId}),
-//            // our data is an array of objects, in the root node instead of /data node, so we need 'dataSrc' parameter
-//            dataSrc : ''
-//        },
-//        columns: columnDefs,
-//        dom: 'Bfrtip',        // Needs button container
-//        select: 'single',
-//        responsive: true,
-//        altEditor: true,     // Enable altEditor
-//        buttons: [
-//            {
-//                text: 'Новый',
-//                name: 'add'        // do not change name
-//            },
-//            {
-//                extend: 'selected', // Bind to Selected row
-//                text: 'Изменить данные',
-//                name: 'edit'        // do not change name
-//            },
-//            {
-//                extend: 'selected', // Bind to Selected row
-//                text: 'Удалить',
-//                name: 'delete'      // do not change name
-//            },
-//            {
-//                text: 'Обновить таблицу',
-//                name: 'refresh'      // do not change name
-//            }
-//        ],
-//
-//        onAddRow: function(datatable, rowdata, success, error) {
-//
-//        var position = {serviceId: rowdata.serviceId,
-//                        quantity : rowdata.quantity,
-//                        totalPrice : rowdata.totalPrice};
-//
-//            $.ajax({
-//                url: '/positions/request?' + $.param({orderId: ordersId}),
-//                type: 'POST',
-//                contentType: "application/json",
-//                data: JSON.stringify(position),
-//                success: success,
-//                error: error
-//            });
-//        },
-//
-//        onDeleteRow: function(datatable, rowdata, success, error) {
-//            $.ajax({
-//                url: '/positions/request?' + $.param({id: rowdata.id}), // выдает null
-//                type: 'DELETE',
-//                success: success,
-//                error: error
-//            });
-//        },
-//
-//        onEditRow: function(datatable, rowdata, success, error) {
-//
-//            var position = {id: rowdata.id,
-//                            serviceId: rowdata.serviceId,
-//                            quantity : rowdata.quantity,
-//                            totalPrice : rowdata.totalPrice};
-//
-//            $.ajax({
-//                url: '/positions/request?' + $.param({orderId: ordersId}),
-//                type: 'PUT',
-//                contentType: "application/json",
-//                data: JSON.stringify(position),
-//                success: success,
-//                error: error
-//            });
-//        }
-//    });
-//
-//
-//
 
 
 
@@ -440,10 +317,127 @@ $(document).ready(function() {
 
 
 
+    var services;
+    var serviceMap = new Map();
 
+    $.ajax({
+            type: 'GET',
+            url: "/services/request",
+            async: false,
+            success: function (response) {services = response;}
+            });
 
+    services.forEach(function(service){
+        serviceMap.set(service.id, service.serviceType);
+        })
 
+    var servicesOptions = Object.fromEntries(serviceMap.entries());
 
+    var serviceColumns = [
+
+        {
+        data: "id",
+        title: "id",
+        type: "readonly"
+        },
+        {
+        data: "serviceId",
+        title: "Услуга",
+        type : "select",
+        options : servicesOptions,
+        select1 : { width: "100%"},
+        render: function (data, type, row, meta) {
+            if (data == null || !(data in servicesOptions)) {
+            return null;
+            }
+            return servicesOptions[data];
+            }
+        },
+        {
+        data: "quantity",
+        title: "Количество",
+        type: "number",
+        required: true,
+        },
+        {
+        data: "totalPrice",
+        title: "сумма",
+        },
+    ];
+
+    var serviceTable;
+    var servicesArr = [];
+    var serviceFrontId = 1;
+
+    serviceTable = $('#positions').DataTable({
+        "sPaginationType": "full_numbers",
+
+        data: servicesArr,
+        columns: serviceColumns,
+        dom: 'Bfrtip',        // Needs button container
+        select: 'single',
+        responsive: true,
+        altEditor: true,     // Enable altEditor
+        buttons: [
+            {
+                text: 'Новый',
+                name: 'add'        // do not change name
+            },
+            {
+                extend: 'selected', // Bind to Selected row
+                text: 'Изменить данные',
+                name: 'edit'        // do not change name
+            },
+            {
+                extend: 'selected', // Bind to Selected row
+                text: 'Удалить',
+                name: 'delete'      // do not change name
+            }
+        ],
+
+        onAddRow: function(datatable, rowdata, success, error) {
+
+                newId = "new" + serviceFrontId.toString();
+                theRow = {id: newId,
+                          serviceId: rowdata["serviceId"],
+                          stuffId: rowdata["stuffId"],
+                          quantity : rowdata["quantity"],
+                          totalPrice : rowdata["totalPrice"]
+                          };
+
+                serviceFrontId += 1;
+
+                servicesArr.push(theRow);
+                updateResultTable();
+                success(theRow);
+
+        },
+
+        onDeleteRow: function(datatable, rowdata, success, error) {
+
+            const index = servicesArr.findIndex(n => n.id === rowdata["id"]);
+            if (index !== -1) {servicesArr.splice(index, 1);}
+
+            updateResultTable();
+            success(rowdata);
+
+        },
+
+        onEditRow: function(datatable, rowdata, success, error) {
+
+            const index = servicesArr.findIndex(n => n.id === rowdata["id"]);
+
+            theRow = {id: newId,
+                      serviceId: rowdata["serviceId"],
+                      stuffId: rowdata["stuffId"],
+                      quantity : rowdata["quantity"],
+                      totalPrice : rowdata["totalPrice"]
+                      };
+
+            servicesArr[index] =  theRow;
+            success(rowdata);
+        }
+    });
 
 
 
