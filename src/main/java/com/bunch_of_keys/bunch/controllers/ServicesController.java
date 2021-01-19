f1f12d5495d66786099f2d4b179aaf599ed8ea1d
isgleb <71262423+isgleb@users.noreply.github.com>
2021-01-19 05:35:44 +0300

    spring data rest
    tried with cleaning services

--- src/main/java/com/bunch_of_keys/bunch/controllers/ServicesController.java
+++ src/main/java/com/bunch_of_keys/bunch/controllers/ServicesController.java
@@ -1,46 +1,46 @@
-package com.bunch_of_keys.bunch.controllers;
-
-import com.bunch_of_keys.bunch.dto.ServiceDto;
-import com.bunch_of_keys.bunch.services.CleaningServService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-// @RestController работает как @Controller но как бы добавляет к каждому методу аннотацию  @ResponceBody
-// аннотации @ResponceBody конвертирует java объект в JSON, @RequestBody конвертирует входной JSON в  объект Java
-
-@RestController
-public class ServicesController {
-
-    @Autowired
-    private CleaningServService cleaningServService;
-
-    @GetMapping("/services/request")
-    public ResponseEntity getServices () {
-
-        List<ServiceDto> serviceDtos = cleaningServService.getServices();
-
-
-
-        return new ResponseEntity(serviceDtos, HttpStatus.OK);
-    }
-
-    @PostMapping("/services/request")
-    public ResponseEntity newService (@RequestBody ServiceDto serviceDto) {
-        cleaningServService.addService(serviceDto);
-        return new ResponseEntity(serviceDto, HttpStatus.OK);
-    }
-
-    @DeleteMapping("/services/request")
-    public void deleteService (@RequestParam Long id) { // работает с postman
-        cleaningServService.deleteService(id);
-    }
-
-    @PutMapping("/services/request")
-    public ResponseEntity editService (@RequestBody ServiceDto serviceDto) {
-        return new ResponseEntity(cleaningServService.editService(serviceDto), HttpStatus.OK);
-    }
-}
+//package com.bunch_of_keys.bunch.controllers;
+//
+//import com.bunch_of_keys.bunch.dto.ServiceDto;
+//import com.bunch_of_keys.bunch.services.CleaningServService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.List;
+//
+//// @RestController работает как @Controller но как бы добавляет к каждому методу аннотацию  @ResponceBody
+//// аннотации @ResponceBody конвертирует java объект в JSON, @RequestBody конвертирует входной JSON в  объект Java
+//
+//@RestController
+//public class ServicesController {
+//
+//    @Autowired
+//    private CleaningServService cleaningServService;
+//
+//    @GetMapping("/services/request")
+//    public ResponseEntity getServices () {
+//
+//        List<ServiceDto> serviceDtos = cleaningServService.getServices();
+//
+//
+//
+//        return new ResponseEntity(serviceDtos, HttpStatus.OK);
+//    }
+//
+//    @PostMapping("/services/request")
+//    public ResponseEntity newService (@RequestBody ServiceDto serviceDto) {
+//        cleaningServService.addService(serviceDto);
+//        return new ResponseEntity(serviceDto, HttpStatus.OK);
+//    }
+//
+//    @DeleteMapping("/services/request")
+//    public void deleteService (@RequestParam Long id) { // работает с postman
+//        cleaningServService.deleteService(id);
+//    }
+//
+//    @PutMapping("/services/request")
+//    public ResponseEntity editService (@RequestBody ServiceDto serviceDto) {
+//        return new ResponseEntity(cleaningServService.editService(serviceDto), HttpStatus.OK);
+//    }
+//}
--- src/main/java/com/bunch_of_keys/bunch/domain/bills/CleaningServiceRepository.java
+++ src/main/java/com/bunch_of_keys/bunch/domain/bills/CleaningServiceRepository.java
@@ -2,8 +2,9 @@ package com.bunch_of_keys.bunch.domain.bills;
 
 import com.bunch_of_keys.bunch.domain.bills.CleaningService;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-
+@RepositoryRestResource(path = "cleaning-services")
 public interface CleaningServiceRepository extends JpaRepository<CleaningService, Long> {
 
 }
